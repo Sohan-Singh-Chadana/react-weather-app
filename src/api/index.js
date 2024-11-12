fd0395189f9a82e33f88602df57284a091f5b750
Sohan Singh <sohansinghchadana83@gmail.com>
2024-11-13 03:59:31 +0530

    Api key Update

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_KEY = process.env.REACT_APP_API_KEY;
+// const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = process.env.REACT_APP_TEST_API;
 
 export async function getWeatherData(endpoint, place_id, measurementSystem) {
   const options = {
